52fdaed96167c8ab9250620f5ecece982af2ac20
Ramesh Kumar S <75178176+Ramesh-kumar-S@users.noreply.github.com>
2021-02-16 23:56:34 +0530

    Added an Email Generator Script
    A Python Script that will Automatically Generate E-Mail ID's for all the students based on their Name,Department ,Batch Year ,Domain Name of their Instituition.

---
+++ Email generator.py
@@ -0,0 +1,37 @@
+import time
+
+#Fucntion Passes the Count of Number of Mail Id's to be Genrated
+def emailgenerator(count):
+    c=count
+    return gen(count)
+
+
+#Fuction generates E-Mails
+def gen(count):
+    MAILBOX=[]
+    for i in range(count):
+        NAME_SPACED= input("Enter your Name :").lower().split()
+        NAME="".join(NAME_SPACED)
+        INITIAL=input("Enter your Initial :").lower()
+        BATCH_FULL=input("Enter your Batch :")
+        BATCH=BATCH_FULL[-2:]
+        DEPT=str(input("Enter you Department :")).lower()
+        DOMAIN=input("Enter the Domain  Name Given by your E-Mail Provider :").lower()
+        TLD=input("Enter your TLD Name : ").lower()
+        email=str(NAME)+str(INITIAL)+"."+str(BATCH)+str(DEPT)+"@"+DOMAIN+TLD
+        MAILBOX.append(email)
+        REMAINING=int(count)-int(i+1)
+        print("{} Mail Id's to be Genrated".format(REMAINING))
+        time.sleep(2)
+    return mailprinter(MAILBOX)
+
+
+#Fucntion Prints the  Generrated Mail Id's
+def mailprinter(MAILBOX):
+    for i in MAILBOX:
+        time.sleep(1)
+        print(i)
+
+#Driver Code
+NO=int(input("Enter the No of Emails to be Generated :"))
+emailgenerator(NO)
