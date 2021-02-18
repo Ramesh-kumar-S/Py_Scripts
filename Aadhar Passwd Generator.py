04fc3f7fcf81726a4e492828bcde1898b79fd068
Ramesh Kumar S <75178176+Ramesh-kumar-S@users.noreply.github.com>
2021-02-18 23:50:29 +0530

    A Simple E-Aadhar Password generator
    A Scriot that will get an Input like Name and Date of birth , Which you have Given to Aadhar and Generates and Password to open the Downloaded E-Aadhar PDF File.

---
+++ Aadhar Passwd Generator.py
@@ -0,0 +1,37 @@
+import time
+
+
+def counter(count):
+    return Generator(int(count))
+
+def Generator(count):
+    
+    SPLITTER=[]
+    for i in range(count):
+        NAME=input("Enter your Name :")
+        DOB=input("Enter the Date of Birth :")
+        NAMES_SPLITTED=NAME[:4].upper()
+        DOB_SPLITTED=DOB[-4:]
+        PASSWD=NAMES_SPLITTED+DOB_SPLITTED
+        SPLITTER.append(PASSWD)
+    decorator()
+    return Printer(SPLITTER)
+
+
+def decorator():
+    str="*"
+    for i in range(10):
+        print(i*str,end="")
+        
+def Printer(SPLITTER):
+    for i in SPLITTER:
+        print("\nYour Password is : {}".format(i))
+        time.sleep(1)
+    decorator()
+
+
+#NAME=input("Enter your Name :")
+#DOB=input("Enter the Date of Birth :")
+
+COUNT=input("Enter the Number of Passwords to be Generated :")
+counter(COUNT)
